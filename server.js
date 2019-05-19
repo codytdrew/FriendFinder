7c24d2c293eb25b101d0bbb6cbc2409134c8863c
John Florence <codytdrew@gmail.com>
2019-05-19 14:58:47 -0700

    Express application setup has begun

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 // Declaration of dependencies - Path and Express
+// ==============================================
 var express = require('express');
 var path = require('path');
+
+// Set up of the Express App
+// ==============================================
+var app = express();
+var PORT = 8080;
+
+// Set up of Express app to handle data parsing
+// ==============================================
