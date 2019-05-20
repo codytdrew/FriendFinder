21f625872ada8213ab4f93b8a2c19114a9c873e0
John Florence <codytdrew@gmail.com>
2019-05-19 21:34:36 -0700

    Bootstrap Added

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+// Routes
+// ========================================
+app.get("/", function(req, res) {
+    res.send("Welcome to Friend Finder.  The future of friendship at your fingertips!");
+});
+
+// app.listener(PORT, function (){
+//     console.log("App listening on PORT " + PORT);
+// });
