0abb7c91aeb409ec6f9b15e3f21c723048cfbe6a
John Florence <codytdrew@gmail.com>
2019-05-22 20:08:13 -0700

    friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,390 @@
 
 var friendArray = [
     {
-      "name": "Emily Ratajkowski",
-      "photo": "https://images.app.goo.gl/PAVGHQUPoRQvcLKM6",
-      "scores": [
-          10,
-          10,
-          10,
-          10,
-          10,
-          10,
-          10,
-          10,
-          10,
-          10,
-      ]  
+        "name": "Emily Ratajkowski",
+        "photo": "https://images.app.goo.gl/PAVGHQUPoRQvcLKM6",
+        "scores": [
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+            "10",
+        ]
     },
+    {
+        "name": "Scarlett Johansson",
+        "photo": "https://images.app.goo.gl/5mKBN78cyGCMc8JH6",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Emilia Clarke",
+        "photo": "https://images.app.goo.gl/Kj4S6HPY7TeDB1YCA",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Mila Kunis",
+        "photo": "https://images.app.goo.gl/JYToVoxkVtPGfDcCA",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Megan Fox",
+        "photo": "https://images.app.goo.gl/BdsoAYfGN37fSdNM6",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Selena Gomez",
+        "photo": "https://images.app.goo.gl/Qepp3RngiXFqtMod7",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Emma Watson",
+        "photo": "https://images.app.goo.gl/MEn4ooUUjDfeS8Lv9",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Kate Upton",
+        "photo": "https://images.app.goo.gl/fwRwGCb3wpxRq5zLA",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "5",
+            "2",
+            "3",
+            "4",
+            "3",
+            "5",
+            "3"
+        ]
+    },
+    {
+        "name": "Jennifer Lawrence",
+        "photo": "https://images.app.goo.gl/fx37Q9XTxHArvJyG6",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Elizabeth Olsen",
+        "photo": "https://images.app.goo.gl/diDVtKSAZs8HwaWMA",
+        "scores": [
+            "3",
+            "3",
+            "1",
+            "1",
+            "3",
+            "4",
+            "3",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Olivia Munn",
+        "photo": "https://images.app.goo.gl/GodtYGnH13peLNzJ7",
+        "scores": [
+            "3",
+            "3",
+            "1",
+            "1",
+            "3",
+            "4",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Anna Kendrick",
+        "photo": "https://images.app.goo.gl/pLcfYoyWYN67YCaa98",
+        "scores": [
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "5",
+            "3",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Isla Fisher",
+        "photo": "https://images.app.goo.gl/VnhHu4epSP3m8CWy7",
+        "scores": [
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "5",
+            "3",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Shakira",
+        "photo": "https://images.app.goo.gl/YERdZTp1DVfrfeGY6",
+        "scores": [
+            "4",
+            "3",
+            "2",
+            "2",
+            "4",
+            "4",
+            "3",
+            "3",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Vanessa Hudgens",
+        "photo": "https://images.app.goo.gl/3U66Jys1BHQdD9t7A",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "4",
+            "3",
+            "3",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Anne Hathaway",
+        "photo": "https://images.app.goo.gl/SwGgvNxBiM1tbpeHA",
+        "scores": [
+            "2",
+            "1",
+            "3",
+            "4",
+            "3",
+            "2",
+            "3",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Emma Stone",
+        "photo": "https://images.app.goo.gl/kmXhsiHqKZZZAmk79",
+        "scores": [
+            "5",
+            "3",
+            "5",
+            "4",
+            "4",
+            "4",
+            "1",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Hayden Panettiere",
+        "photo": "https://images.app.goo.gl/4KvCxYvG5JWpzrs86",
+        "scores": [
+            "4",
+            "4",
+            "5",
+            "3",
+            "3",
+            "2",
+            "4",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        "name": "Arianny Celeste",
+        "photo": "https://images.app.goo.gl/tHxcTowQx9or255f7",
+        "scores": [
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Alice Eve",
+        "photo": "https://images.app.goo.gl/UvaCXfe8d2EBb2xT8",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Yvonne Strahovski",
+        "photo": "https://images.app.goo.gl/BtuvTf9YEWcuGPJk6",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Taylor Swift",
+        "photo": "https://images.app.goo.gl/1rxu2GCNbETPSW3U7",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Cara Delevingne",
+        "photo": "https://images.app.goo.gl/y3FpbQQh78XtJQRe6",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Sofia Vergara",
+        "photo": "https://images.app.goo.gl/tzGjX8k5TSRGNURYA",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
 
+        ],
+    }
 ];
 
 // Exports the array so that other files can access with require
